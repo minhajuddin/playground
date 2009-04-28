88a570312bcf6e58e1dc2990979fce046c921e4f
Khaja Minhajuddin <minhajuddin.k@hotmail.com>
2009-04-28 16:17:37 +0530

    added a jquery directory

--- MVC/Scratch.MVC/Scratch.MVC/Controllers/HomeController.cs
+++ MVC/Scratch.MVC/Scratch.MVC/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Scratch.MVC.Controllers {
     [HandleError]
     public class HomeController : Controller {
         public ActionResult Index() {
-            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+            ViewData["Message"] = "Welcome to ASP.NET MVC!!";
 
             return View();
         }
