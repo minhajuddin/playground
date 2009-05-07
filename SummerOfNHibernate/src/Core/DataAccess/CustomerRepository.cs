b2d40c492b27716f3f60272b2e888b821e900ebf
Khaja Minhajuddin <minhajuddin.k@hotmail.com>
2009-05-07 23:26:41 +0530

    Added a method using HQL

--- SummerOfNHibernate/src/Core/DataAccess/CustomerRepository.cs
+++ SummerOfNHibernate/src/Core/DataAccess/CustomerRepository.cs
@@ -1,18 +1,27 @@
 using Core.Domain;
 using NHibernate;
 using NHibernate.Cfg;
+using System.Collections.Generic;
 
 namespace Core.DataAccess {
     public class CustomerRepository {
 
         public Customer GetCustomerById(int id) {
-            //var config = new Configuration();
-            //config.Configure();
-            //ISessionFactory sessionFactory = config.BuildSessionFactory();
-            ISessionFactory sessionFactory = (new Configuration()).Configure().BuildSessionFactory();
-            ISession session = sessionFactory.OpenSession();
+            ISession session = GetSession();
             var customer = session.Get<Customer>(id);
             return customer;
+        }
+
+        public IList<Customer> GetCustomersByFirstName(string firstName) {
+            ISession session = GetSession();
+            var customers = session.CreateQuery("select from Customer c where c.FirstName ='" + firstName + "'").List<Customer>();
+            return customers;
+        }
+
+        //Helper methods
+        private static ISession GetSession() {
+            ISessionFactory sessionFactory = (new Configuration()).Configure().BuildSessionFactory();
+            return sessionFactory.OpenSession();
         }
 
     }
--- SummerOfNHibernate/src/UnitTests/DataAccess/CustomerRepositoryTests.cs
+++ SummerOfNHibernate/src/UnitTests/DataAccess/CustomerRepositoryTests.cs
@@ -1,15 +1,32 @@
 using MbUnit.Framework;
 using Core.DataAccess;
+using Core.Domain;
+using System.Collections.Generic;
 
 namespace UnitTests.DataAccess {
     [TestFixture]
     public class CustomerRepositoryTests {
+        CustomerRepository _repo;
+
+        [FixtureSetUp]
+        public void FixtureSetup() {
+            _repo = new CustomerRepository();
+        }
+
         [Test]
         public void GetCustomerByIdReturnsACustomer() {
-            var repo = new CustomerRepository();
-            var customer = repo.GetCustomerById(1);
+            var customer = _repo.GetCustomerById(1);
             Assert.IsNotNull(customer);
             Assert.AreEqual(1, customer.ID);
+        }
+
+        [Test]
+        public void GetCustomerByFirstNameShouldReturnAnIListOfCustomerWithAMatchingFirstName() {
+            var customers = _repo.GetCustomersByFirstName("Khaja");
+
+            Assert.IsNotNull(customers);
+            Assert.IsInstanceOfType(typeof(IList<Customer>), customers);
+            Assert.IsTrue(customers.Count > 0);
         }
     }
 }
