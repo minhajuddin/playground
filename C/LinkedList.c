1c0c759b4828e3055b5574df5f5c0247f17ac44d
Khaja Minhajuddin <minhajuddin.k@hotmail.com>
2009-05-10 15:00:00 +0530

    Added a folder for C Code snippets

---
+++ C/LinkedList.c
@@ -0,0 +1,12 @@
+#inlcude <stdio.h>
+
+/* function prototypes  */
+void add_at_end();
+
+/* Template for a node in the linked list  */
+struct Node{
+  int data;
+  struct Node *next_node;
+}
+
+
---
+++ C/helloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void test_fun(){
+	printf("In test function");
+}
+
+void main(){
+	test_fun();
+}
