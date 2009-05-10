f9364c31ea371b01a2bc54842f40a18d747698fb
Khaja Minhajuddin <minhajuddin.k@hotmail.com>
2009-05-10 18:08:21 +0530

    Finished the silverlight scratch stuff

---
+++ Silverlight/SilverlightScratch/SilverlightScratch/DiggStory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SilverlightScratch {
+    public class DiggStory {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int NumDiggs { get; set; }
+        public Uri HrefLink { get; set; }
+        public string Thumbnail { get; set; }
+        public string UserName { get; set; }
+    }
+}
